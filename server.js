42fc9ffc9783a141402651d612674347b1c5d789
vishuhanda001 <vishuhanda001@gmail.com>
2016-12-27 23:38:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const api = require('./server/routes/api');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+app.use('/api', api);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+let device;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+ deviceinfo()
+ {
+   document.addEventListener('deviceready', () => {
+      console.log('Using Cordova plugins with Angular 2. Cordova version: ' + device.cordova)
+    }, false)
+ }
+
 }
